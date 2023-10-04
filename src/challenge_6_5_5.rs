d04730b1c1c543e0508d18499b97415397b6b9bf
EFanZh <efanzh@gmail.com>
2023-10-04 12:57:29 +0800

    Add challenge 6.5 - 5

---
+++ src/challenge_6_5_5.rs
@@ -0,0 +1,36 @@
+use crate::challenge_5_8_4::Either;
+
+pub fn sum_to_product<T>(value: Either<T, T>) -> (bool, T) {
+    match value {
+        Either::Left(value) => (false, value),
+        Either::Right(value) => (true, value),
+    }
+}
+
+pub fn product_to_sum<T>(value: (bool, T)) -> Either<T, T> {
+    if value.0 {
+        Either::Right(value.1)
+    } else {
+        Either::Left(value.1)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::challenge_5_8_4::Either;
+    use crate::challenge_6_5_5::{product_to_sum, sum_to_product};
+    use fn_traits::{fns, Fn};
+
+    #[test]
+    fn test_isomorphism() {
+        let id_sum = fns::compose(sum_to_product, product_to_sum);
+
+        assert!(matches!(id_sum.call((Either::Left(2),)), Either::Left(2)));
+        assert!(matches!(id_sum.call((Either::Right(3),)), Either::Right(3)));
+
+        let id_product = fns::compose(product_to_sum, sum_to_product);
+
+        assert_eq!(id_product.call(((false, 2),)), (false, 2));
+        assert_eq!(id_product.call(((true, 3),)), (true, 3));
+    }
+}
