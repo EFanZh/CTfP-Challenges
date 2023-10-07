db2faa7c5eb67d64b51f0c6225cb4c0b5a1a0bce
EFanZh <efanzh@gmail.com>
2023-10-07 22:31:06 +0800

    Refactor Functor

--- src/concepts/functor.rs
+++ src/concepts/functor.rs
@@ -1,7 +1,11 @@
 use fn_traits::FnMut;
 
-pub trait Functor<T> {
-    type FMap<F>: Functor<F::Output>
+pub trait Functor<C, T>: Sized {
+    type FMapOutput<F>: Functor<C, F::Output>
+    where
+        F: FnMut<(T,)>;
+
+    type FMap<F>: FnMut<(Self,), Output = Self::FMapOutput<F>>
     where
         F: FnMut<(T,)>;
 
@@ -10,28 +14,73 @@ pub trait Functor<T> {
         F: FnMut<(T,)>;
 }
 
-impl<T> Functor<T> for Option<T> {
-    type FMap<F> = Option<F::Output>
-    where
-        F: FnMut<(T,)>;
+pub struct OptionFunctor;
 
-    fn fmap<F>(self, mut f: F) -> Self::FMap<F>
-    where
-        F: FnMut<(T,)>,
-    {
-        self.map(|value| f.call_mut((value,)))
+pub struct OptionFMap<F> {
+    f: F,
+}
+
+impl<T, F> FnMut<(Option<T>,)> for OptionFMap<F>
+where
+    F: FnMut<(T,)>,
+{
+    type Output = Option<F::Output>;
+
+    fn call_mut(&mut self, args: (Option<T>,)) -> Self::Output {
+        args.0.map(|value| self.f.call_mut((value,)))
     }
 }
 
-impl<T> Functor<T> for Vec<T> {
-    type FMap<F> = Vec<F::Output>
+impl<T> Functor<OptionFunctor, T> for Option<T> {
+    type FMapOutput<F> = Option<F::Output>
     where
         F: FnMut<(T,)>;
 
-    fn fmap<F>(self, mut f: F) -> Self::FMap<F>
+    type FMap<F> = OptionFMap<F>
+    where
+        F: FnMut<(T,)>;
+
+    fn fmap<F>(self, f: F) -> Self::FMap<F>
     where
         F: FnMut<(T,)>,
     {
-        self.into_iter().map(|value| f.call_mut((value,))).collect()
+        OptionFMap { f }
+    }
+}
+
+pub struct VecFunctor;
+
+pub struct VecFMap<F> {
+    f: F,
+}
+
+impl<T, F> FnMut<(Vec<T>,)> for VecFMap<F>
+where
+    F: FnMut<(T,)>,
+{
+    type Output = Vec<F::Output>;
+
+    fn call_mut(&mut self, args: (Vec<T>,)) -> Self::Output {
+        args.0
+            .into_iter()
+            .map(|value| self.f.call_mut((value,)))
+            .collect()
+    }
+}
+
+impl<T> Functor<VecFunctor, T> for Vec<T> {
+    type FMapOutput<F> = Vec<F::Output>
+    where
+        F: FnMut<(T,)>;
+
+    type FMap<F> = VecFMap<F>
+    where
+        F: FnMut<(T,)>;
+
+    fn fmap<F>(self, f: F) -> Self::FMap<F>
+    where
+        F: FnMut<(T,)>,
+    {
+        VecFMap { f }
     }
 }
