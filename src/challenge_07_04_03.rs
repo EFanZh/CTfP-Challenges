90c7656a12f16428afd9674de6cdd3f4adbb88e7
EFanZh <efanzh@gmail.com>
2023-10-17 22:31:20 +0800

    Refactor

--- src/challenge_07_04_03.rs
+++ src/challenge_07_04_03.rs
@@ -53,34 +53,37 @@ where
 
 #[cfg(test)]
 mod tests {
-
+    use super::{Reader, ReaderFunctor};
     use crate::concepts::functor::Functor;
-    use fn_traits::fns::ComposeFn;
     use fn_traits::{fns, FnMut, FnOnce};
     use std::convert;
 
+    fn fmap<F, R, T, U>(
+        f: F,
+    ) -> impl FnOnce<(R,), Output = <R as Functor<ReaderFunctor<T>, U>>::FMapOutput<F>>
+    where
+        R: Reader<T, U>,
+        F: FnMut<(U,)>,
+    {
+        R::fmap(f)
+    }
+
     #[test]
     fn test_reader() {
-        type R = fn(u8) -> u16;
-
         let reader = |x: u8| u16::from(x + 1);
 
         // Preservation of identity.
 
         assert_eq!(
-            R::fmap(convert::identity)
-                .call_once((reader,))
-                .call_mut((2,)),
-            3
+            fmap(convert::identity).call_once((reader,)).call_mut((2,)),
+            3,
         );
 
         assert_eq!(reader(2), 3);
 
         assert_eq!(
-            R::fmap(convert::identity)
-                .call_once((reader,))
-                .call_mut((3,)),
-            4
+            fmap(convert::identity).call_once((reader,)).call_mut((3,)),
+            4,
         );
 
         assert_eq!(reader(3), 4);
@@ -90,10 +93,8 @@ mod tests {
         let f = |x| u32::from(x * 2);
         let g = |x| u64::from(x + 7);
 
-        let mut compose_then_map = R::fmap(fns::compose(f, g)).call_once((reader,));
-
-        let mut map_then_compose =
-            fns::compose(R::fmap(f), ComposeFn::<R, fn(_) -> _>::fmap(g)).call_once((reader,));
+        let mut compose_then_map = fmap(fns::compose(f, g)).call_once((reader,));
+        let mut map_then_compose = fns::compose(fmap(f), fmap(g)).call_once((reader,));
 
         assert_eq!(compose_then_map.call_mut((2,)), 13);
         assert_eq!(map_then_compose.call_mut((2,)), 13);
